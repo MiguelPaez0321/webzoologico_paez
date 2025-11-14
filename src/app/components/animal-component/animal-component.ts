9ede999a552b7aedf5e92f2d700103f9fa8e42f5
MiguelPaez <luism.paezd@konradlorenz.edu.co>
2025-11-13 20:20:23 -0500

    Se realiza crud completo para agregar, editar y eliminar de la base de datos, adicional se realizo adelantamiento

--- src/app/components/animal-component/animal-component.ts
+++ src/app/components/animal-component/animal-component.ts
@@ -12,13 +12,17 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule, ReactiveFormsModule], // 👈 AGREGA ESTO,
   templateUrl: './animal-component.html',
   styleUrl: './animal-component.css',
+
 })
 
 export class AnimalComponent {
   animalList: any = [];
   animalForm: FormGroup | any;
+  idAnimal: any;
+  editableAnimal: boolean = false;
 
-  constructor(private animalService: AnimalService, private toastr: ToastrService, private formBuilder: FormBuilder, private router: Router) { 
+
+  constructor(private animalService: AnimalService, private toastr: ToastrService, private formBuilder: FormBuilder, private router: Router) {
   }
   getAllAnimals() {
     this.animalService.getAllAnimalsData().subscribe((data: {}) => {
@@ -42,18 +46,57 @@ export class AnimalComponent {
       .subscribe(() => window.location.reload());
   }
 
-  newAnimalEntry(){
+  newAnimalEntry() {
     this.animalService.newAnimal(this.animalForm.value).subscribe(
 
       () => {
         //Redirigiendo a la ruta actual /animales y recargando la ventana
         this.router.navigate(['/animales'])
-        .then(() => {
-          this.newMessage('Registro exitoso');
-                this.animalForm.reset();
-          this.getAllAnimals(); // 🔄 actualiza la lista sin recargar
-        })
+          .then(() => {
+            this.newMessage('Registro exitoso');
+            this.animalForm.reset();
+            this.getAllAnimals(); // 🔄 actualiza la lista sin recargar
+          })
       }
     );
   }
+  updateAnimalEntry() {
+    //Removiendo valores vacios del formulario de actualización
+    for (let key in this.animalForm.value) {
+      if (this.animalForm.value[key] === '') {
+        this.animalForm.removeControl(key);
+      }
+    }
+    this.animalService.updateAnimal(this.idAnimal, this.animalForm.value).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Animal editado");
+      }
+    );
+  }
+  toggleEditAnimal(id: any) {
+    this.idAnimal = id;
+    console.log(this.idAnimal)
+    this.animalService.getOneAnimal(id).subscribe(
+      data => {
+        this.animalForm.setValue({
+          nombre: data.nombre,
+          edad: data.edad,
+          tipo: data.tipo,
+        });
+      }
+    );
+    this.editableAnimal = !this.editableAnimal;
+  }
+  deleteAnimalEntry(id: any) {
+    console.log(id)
+    this.animalService.deleteAnimal(id).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Animal eliminado");
+      }
+    );
+  }
+
+
 }
